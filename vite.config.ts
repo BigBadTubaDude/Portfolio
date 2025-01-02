2dc10faf04a2650b48014ddd3011e3467401217e
BigBadTubaDude <cralex0307@gmail.com>
2025-01-02 18:32:34 -0500

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { compression } from 'vite-plugin-compression2'
+// import { compression } from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Portfolio",
-  plugins: [react(), compression({
-    algorithm: "brotliCompress",
-  })],
+  plugins: [react(), 
+    // compression({
+    // algorithm: "brotliCompress",
+  // })
+],
 })
